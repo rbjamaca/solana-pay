8638b3fefbefc389df65335fed37fc1b21f7decf
Ross Bernard Jamaca <rossbernard.jamaca@fligno.com>
2022-08-22 15:54:15 +0800

    build: devnet

--- pages/api/createTransaction.js
+++ pages/api/createTransaction.js
@@ -4,8 +4,8 @@ import { createTransferCheckedInstruction, getAssociatedTokenAddress, getMint } 
 import BigNumber from "bignumber.js";
 import products from "./products.json";
 
-const usdcAddress = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
-const sellerAddress = "8PhtNrzahsigzHNbwMvFtLP9uY4DkXz5MFjet5AvctCy";
+const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
+const sellerAddress = "4mTkpBSB7hYpkpcLTCaRRFF6mAnJi55Hsyz1CCvtVhmj";
 const sellerPublicKey = new PublicKey(sellerAddress);
 
 const createTransaction = async (req, res) => {
